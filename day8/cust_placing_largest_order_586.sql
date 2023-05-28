a3de892784f4d046f38646f7e3ae1c7ce7916da2
Rachata Punyathananan <rachata.p@KB-21A3K074KQ.kasikornbank.com>
2023-05-28 20:07:57 +0700

    update 586. after seen some solve

--- day8/cust_placing_largest_order_586.sql
+++ day8/cust_placing_largest_order_586.sql
@@ -1,11 +1,5 @@
-SELECT 
-    c.customer_number
-FROM (
-    SELECT 
-        customer_number, 
-        COUNT(*) AS num_duplicates
-    FROM Orders
-    GROUP BY customer_number
-    ORDER BY num_duplicates DESC
-    LIMIT 1
-) c
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
